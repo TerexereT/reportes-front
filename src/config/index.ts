a44a96d03ef793466dece02e30300ce3edd6eb9a
Armando Rivas <armandojrivasc@gmail.com>
2021-09-14 14:01:58 -0400

    Axios to use apiurl

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // Set config defaults when creating the instance
-
+const API_URL = `10.198.73.15:4040`;
 const useAxios = axios.create({
-	baseURL: 'http://10.198.68.21:5050',
+	baseURL: process.env.NODE_ENV === 'development' ? `http://10.198.68.21:4040` : API_URL,
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
