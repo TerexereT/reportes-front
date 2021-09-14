ffb0cad1b012eb64ad62f4fe423e598739e2770a
Armando Rivas <armandojrivasc@gmail.com>
2021-09-14 14:48:26 -0400

    bug fix axios config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set config defaults when creating the instance
-const API_URL = `10.198.73.15:4040`;
+const API_URL = `http://10.198.73.15:4040`;
 const useAxios = axios.create({
 	baseURL: process.env.NODE_ENV === 'development' ? `http://10.198.68.21:4040` : API_URL,
 	headers: { common: { token: localStorage.getItem('token') } },
