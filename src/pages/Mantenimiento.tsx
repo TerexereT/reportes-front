cff2452de55557bd1038815c725362514e4eedc5
Armando Rivas <armandojrivasc@gmail.com>
2021-09-15 11:51:56 -0400

    date picker para mantenimiento, log deleted

--- src/pages/Mantenimiento.tsx
+++ src/pages/Mantenimiento.tsx
@@ -2,21 +2,25 @@
 import { Card } from '@material-ui/core';
 import React, { Fragment } from 'react';
 import CheckboxList from '../components/CheckboxList';
+import SelectList from '../components/DateTime';
 import TableReports from '../components/table';
 import useAxios from '../config';
 import { useStyles } from './RepDinamicos';
-// import SelectList from '../components/DateTime';
 
 const Mantenimiento: React.FC = () => {
 	const classes = useStyles();
-	const [state, setState]: [any, any] = React.useState({});
+	const today = new Date();
+	const lastMonth = new Date(today);
+
+	const [state, setState] = React.useState({});
+	const [initDate, setInitDate] = React.useState<Date | null>(lastMonth);
+	const [endDate, setEndDate] = React.useState<Date | null>(today);
 
 	React.useEffect(() => {
 		const getdata = async () => {
 			try {
 				const resp = await useAxios.get('/aboterminal/keys');
 				setState(resp.data.info);
-				console.log('aboterminal', state);
 			} catch (error) {}
 		};
 		getdata();
@@ -26,13 +30,13 @@ const Mantenimiento: React.FC = () => {
 			<div className='ed-container'>
 				<div className='ed-item s-py-2'>
 					<Card className={classes.card}>
-						{/* <SelectList initDate={initDate} endDate={endDate} setInitDate={setInitDate} setEndDate={setEndDate} /> */}
+						<SelectList initDate={initDate} endDate={endDate} setInitDate={setInitDate} setEndDate={setEndDate} />
 
 						<CheckboxList state={state} setState={setState} />
 					</Card>
-					<div className='ed-item s-to-center s-py-2'>
-						<TableReports state={state} from='Mantenimiento' />
-					</div>
+				</div>
+				<div className='ed-item s-to-center s-py-2'>
+					<TableReports state={state} from='Mantenimiento' />
 				</div>
 			</div>
 		</Fragment>
