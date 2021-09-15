6c904c11a16bdf7b1605afd3760b2fa4914590ae
Armando Rivas <armandojrivasc@gmail.com>
2021-09-15 09:13:31 -0400

    warning hide on axios config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // Set config defaults when creating the instance
 const API_URL = `http://10.198.73.15:4040`;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const dev = 'http://10.198.68.21:4040';
 const useAxios = axios.create({
 	baseURL: process.env.NODE_ENV === 'development' ? API_URL : API_URL,
