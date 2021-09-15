aea40603ebe69bc7701c8ad69e5ad99fd06a5f3b
Armando Rivas <armandojrivasc@gmail.com>
2021-09-15 09:11:49 -0400

    api url para dev y prod, variable de dev establecida pero sin uso

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Set config defaults when creating the instance
 const API_URL = `http://10.198.73.15:4040`;
+const dev = 'http://10.198.68.21:4040';
 const useAxios = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? `http://10.198.68.21:4040` : API_URL,
+	baseURL: process.env.NODE_ENV === 'development' ? API_URL : API_URL,
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
