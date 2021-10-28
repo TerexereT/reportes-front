d170b2d953eba6ae5e41e871d0bd26ce4112a5e0
Armando Rivas <arivas@tranred.com.ve>
2021-10-28 14:19:22 +0000

    Update config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ const API_URL = `http://10.198.71.36:4040`;
 const qa = `http://10.198.73.15:4040`;
 const dev = `http://10.198.68.21:4040`;
 const useAxios = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? dev : API_URL,
+	baseURL: process.env.NODE_ENV === 'development' ? dev : qa,
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
