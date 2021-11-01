d09aa68f6a51c45c48a2ded36c9a7f289201bc64
Armando Rivas <armandojrivasc@gmail.com>
2021-11-01 17:41:22 -0400

    create enviroment variable in react

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 // Set config defaults when creating the instance
-const API_URL = `reportedinamico.tranred.com.ve`;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const qa = `http://10.198.73.15:4040`;
+
 const dev = `http://10.198.68.21:4040`;
 const useAxios = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? dev : API_URL,
+	baseURL: process.env.REACT_APP_APIURL ? process.env.REACT_APP_APIURL : dev,
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
 
 export default useAxios;
+
+//REACT_APP_APIURL=http://reportedinamicoqa.tranred.com.ve
