1b8aa58ac556563cc91729b4ff38804a3e7b6d7a
Armando Rivas <armandojrivasc@gmail.com>
2021-11-02 13:13:08 -0400

    para arriba

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,5 +8,5 @@ const useAxios = axios.create({
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
-
+//
 export default useAxios;
