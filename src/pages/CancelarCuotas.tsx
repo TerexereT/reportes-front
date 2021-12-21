b5509736a602dab4b59989859c4ef0970097add6
Armando Rivas <armandojrivasc@gmail.com>
2021-12-21 13:25:54 -0400

    cancelar cuotas

--- src/pages/CancelarCuotas.tsx
+++ src/pages/CancelarCuotas.tsx
@@ -1,7 +1,25 @@
-import { FC } from 'react';
+import { makeStyles, Theme } from '@material-ui/core';
+import { FC, Fragment } from 'react';
+
+const useStyles = makeStyles((theme: Theme) => ({
+	base: {
+		display: 'flex',
+		width: '100%',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: '2rem',
+	},
+}));
 
 const CancelarCuotas: FC = () => {
-	return <div>Esto es Adriana</div>;
+	const classes = useStyles();
+	return (
+		<Fragment>
+			<div className='ed-container'>
+				<div className={classes.base}>Esto es Cancelar Cuotas</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default CancelarCuotas;
