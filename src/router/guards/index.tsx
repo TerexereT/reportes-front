47e9f0a2d0884a8b6f8d2d9e8a7925fcfdd2eb7d
Armando Rivas <armandojrivasc@gmail.com>
2021-12-30 14:56:51 -0400

    guard to redirect home at failed password

--- src/router/guards/index.tsx
+++ src/router/guards/index.tsx
@@ -1,10 +1,13 @@
 import { GuardFunction } from 'react-router-guards';
+import { baseUrl } from '../url';
 
 export const Lock: GuardFunction = (to, from, next) => {
 	if (to.meta.auth) {
 		let pwd = window.prompt('Ingrese Contraseña:');
 		if (pwd === '@drianaTovar#.') {
 			next();
+		} else {
+			next.redirect(baseUrl);
 		}
 	}
 	next();
