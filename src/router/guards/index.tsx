255bea19f1b97394520cf623793b4b6558b9c296
Armando Rivas <armandojrivasc@gmail.com>
2022-01-05 12:17:43 -0400

    cancelarCuotas .env pswd

--- src/router/guards/index.tsx
+++ src/router/guards/index.tsx
@@ -4,7 +4,7 @@ import { baseUrl } from '../url';
 export const Lock: GuardFunction = (to, from, next) => {
 	if (to.meta.auth) {
 		let pwd = window.prompt('Ingrese Contraseña:');
-		if (pwd === '@drianaTovar#.') {
+		if (pwd === (process.env.REACT_APP_PSWD ? process.env.REACT_APP_PSWD : '12345678')) {
 			next();
 		} else {
 			next.redirect(baseUrl);
