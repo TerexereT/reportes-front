a1fa82e97f1428c3313aa9e557b7291c53ff1258
Armando Rivas <armandojrivasc@gmail.com>
2022-04-13 15:06:54 -0400

    PagarCuota front update

--- src/pages/PagoCuota.tsx
+++ src/pages/PagoCuota.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Card } from '@mui/material';
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
 // import CheckboxList from '../components/CheckboxList';
 import SelectList from '../components/DateTime';
 import TableReports from '../components/table';
 import useAxios from '../config';
 import { useStyles } from './RepDinamicos';
 
-const PagoCuota: React.FC = () => {
+const PagoCuota: FC = () => {
 	const classes = useStyles();
 
-	const [state, setState]: [any, any] = React.useState({});
+	const [state, setState] = useState({});
 	const today = new Date();
 	const lastMonth = new Date(today);
-	const [initDate, setInitDate] = React.useState<Date | undefined>(lastMonth);
-	const [endDate, setEndDate] = React.useState<Date | undefined>(today);
+	const [initDate, setInitDate] = useState<Date | undefined>(lastMonth);
+	const [endDate, setEndDate] = useState<Date | undefined>(today);
 
-	React.useEffect(() => {
+	useEffect(() => {
 		const getdata = async () => {
 			try {
 				const resp = await useAxios.get('/pago-cuotas/keys');
