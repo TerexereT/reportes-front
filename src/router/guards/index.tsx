2b9823cbafdd1475611216c4b6a6a65fdafc6099
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-09-22 15:28:30 -0400

    abonoclienterechazadoup

--- src/router/guards/index.tsx
+++ src/router/guards/index.tsx
@@ -2,7 +2,7 @@ import { GuardFunction } from 'react-router-guards';
 import { GuardFunctionRouteProps, GuardToRoute, Next } from 'react-router-guards/dist/types';
 import { Views } from '../../context/auth/interface';
 import { baseUrl, login } from '../url';
-import { isPrivate } from '../utilis/Functions';
+import { isPrivate, route } from '../utilis/Functions';
 
 export const Lock: GuardFunction = (to, from, next) => {
 	// console.log('publicc guarddx', to.location.pathname);
@@ -27,17 +27,19 @@ export const PrivGuard: any = (
 	next: Next,
 	views: Views[] | []
 ) => {
+	// console.log('privv guard xxd', to.location.pathname);
 	if (localStorage.getItem('token') !== null && !isPrivate()) {
-		console.log('vete a homes');
+		//console.log('vete a homes');
 		next.redirect(baseUrl);
 	}
 
 	// console.log('priv guard xxd', to.location.pathname);
 	//console.log('aquii', views);
-	//
+
 	let isWorker = 1;
-	let userDep = views.find((view) => view.root === to.location.pathname.split('/')[1]);
-	//console.log('valid', userDep);
+	let userDep = route(views, to.location.pathname);
+	// console.log(views);
+	// console.log('valid', userDep);
 
 	//
 	if (userDep) next.props({ isWorker });
