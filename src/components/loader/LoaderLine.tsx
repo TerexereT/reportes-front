fc6b8c1ba1fa8b887cb199a687cd36510da8a1a6
Armando Rivas <armandojrivasc@gmail.com>
2022-09-23 16:57:00 -0400

    loaderLine con background del tema

--- src/components/loader/LoaderLine.tsx
+++ src/components/loader/LoaderLine.tsx
@@ -1,26 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Theme } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import LinearProgress from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((styles: Theme) => ({
 	loader: {
 		color: styles.palette.primary.light,
+	},
+	background: {
+		position: 'fixed',
+		top: 0,
+		background: styles.palette.background.paper,
+		width: '100vw',
+		height: '100vh',
+		zIndex: -2,
 	},
 }));
 
 const LoaderLine: React.FC = () => {
 	const classes = useStyles();
 	return (
-		<div
-			style={{
-				width: '100vw',
-				height: '100vh',
-				position: 'relative',
-				padding: 0,
-				margin: 0,
-			}}>
+		<div className={classes.background}>
 			<div
 				style={{
 					position: 'absolute',
