6b0161395d0a8b1f5c303f49c76244267bafe4e1
Armando Rivas <armandojrivasc@gmail.com>
2022-09-26 11:38:45 -0400

    home refactor

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,10 @@
-import { Theme } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import React from 'react';
+import { FC } from 'react';
 import TranredLogo from '../images/tranred-logo.png';
+import { useStyles } from './styles';
 
 interface HomeInt {}
 
-const useStyles = makeStyles((theme: Theme) => ({
-	base: {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		padding: '2rem',
-	},
-	title: {
-		fontSize: 40,
-		padding: '1rem 0',
-		color: theme.palette.text.primary,
-	},
-	subtitle: {
-		fontSize: 24,
-		padding: '1rem',
-		color: theme.palette.text.secondary,
-	},
-}));
-const Home: React.FC<HomeInt> = () => {
+const Home: FC<HomeInt> = () => {
 	const classes = useStyles();
 	return (
 		<>
